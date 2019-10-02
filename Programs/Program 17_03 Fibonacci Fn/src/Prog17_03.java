0434afaa172d918671d4ebabba9f7e3004c252a2
Daniel C Perez <dcperez08@gmail.com>
2019-10-01 21:35:23 -0400

    update cases

--- Programs/Program 17_03 Fibonacci Fn/src/Prog17_03.java
+++ Programs/Program 17_03 Fibonacci Fn/src/Prog17_03.java
@@ -8,20 +8,30 @@ public class Prog17_03{
 }
     
     public Prog17_03(){
-        /*System.out.println("Select your option");
+        System.out.println("Select your option");
         System.out.println("1. Pick a fib");
         System.out.println("2. Fib of first 100 values");
         
         Scanner sn = new Scanner(System.in);
-        int option = sc.nextInt();
+        int option = sn.nextInt();
         
-        Case(option){
+        switch(option){
         
-        1: System.out.println("Enter a value:" );
-            System.out.println(fib());
-    }*/
-        for (int i = 0; i < 100; i++)
+        case 1: 
+                System.out.println("Enter a value:" );
+                int num = sn.nextInt();
+                System.out.println(fib(num));
+                break;
+                
+        case 2:  
+            for (int i = 0; i < 100; i++)
             System.out.println("The fibonacci of " + i + " is: " + fib(i));
+            break;
+            
+        default : 
+            System.out.println("Byyyyyyeeeeeee!");
+            break;
+        }
     }
     
     public int fib(int n){
